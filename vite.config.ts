2552cea6df4045d6c421cd27b4b4f8036dd12f47
Zyrion Play <admin@zyrionplay.com>
2026-05-02 14:07:30 -0300

    Configure base URL for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/adjafnacnandf-anf-na-wana/",
   server: {
     host: "::",
     port: 8080,
